77b2d92d0a05551610489500058a71f1eb7b629f
LG <s2luminous@gmail.com>
2017-02-23 17:56:59 -0500

    create

---
+++ exercises/functions/functions.js
@@ -0,0 +1,16 @@
+var currencyA = 3.14;
+var currencyB = 6.23;
+var currencyC = 9.18;
+
+function foreignExchange(float){
+  USDtoDKK = 7.03;
+  console.log(float * USDtoDKK);
+}
+foreignExchange(currencyA);
+
+
+function foreignExchange2(float){
+  USDtoDKK = 0.14;
+  console.log(float * USDtoDKK);
+}
+foreignExchange2(currencyB);
